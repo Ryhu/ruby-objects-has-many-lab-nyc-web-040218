006a69ce4d5780dfe78f1c370afdf55452bd7ff0
Ryan Hu <ryanhuuu@gmail.com>
2018-04-06 20:30:33 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,18 @@
+<<<<<<< HEAD
 
 class Author
   @@post_count = 0
   attr_accessor :name, :posts
+=======
+class Author
+  attr_accessor :name, :posts
+  
+>>>>>>> afe081150628f78aaaa58571380efd1a9bb18332
   def initialize(name)
     @name = name
     @posts = []
   end
+<<<<<<< HEAD
 
   def add_post(post)
     posts << post
@@ -25,3 +32,10 @@ class Author
 
 
 end
+=======
+  
+  def add_post 
+    
+  end
+end
+>>>>>>> afe081150628f78aaaa58571380efd1a9bb18332
